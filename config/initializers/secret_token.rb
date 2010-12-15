9709842cb629a3dbd3f151c71208e52074867f5f
Brian George <brianwork@brianandsherri.com>
2010-12-14 22:13:32 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MeterReader::Application.config.secret_token = '70d0cf2d4c434ea007f1749848af63b646bff540b9ccb682a9cf555dab744108860cd1372e3b7770bccef97a93b7312ebb2af84ea4d3b17631caa4ed6e7b9ca3'
